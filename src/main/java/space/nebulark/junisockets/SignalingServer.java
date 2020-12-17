a6f002b5705b21d5e2d1f2da62eb8102314e8d2f
Jakob Waibel <jakob.waibel@web.de>
2020-12-17 19:14:37 +0000

    feat: Add dummy function bodies

--- src/main/java/space/nebulark/junisockets/SignalingServer.java
+++ src/main/java/space/nebulark/junisockets/SignalingServer.java
@@ -9,7 +9,7 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint(value = "/chat/{username}")
+@ServerEndpoint(value = "/endpoint")
 public class SignalingServer {
    
     private String host;
@@ -23,21 +23,32 @@ public class SignalingServer {
     @OnOpen
     public void onOpen(Session session) throws IOException {
         // Get Session and WebSocket connection
+        System.out.println("onOpen::" + session.getId());
     }
 
     @OnMessage 
     public void onMessage(Session session, Message message) throws IOException {
         // Handle new Message
+        System.out.println("onMessage::From=" + session.getId() + " Message=" + message);
+
+        try {
+            session.getBasicRemote().sendText("Hello Client " + session.getId() + "!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @OnClose
     public void onClose(Session session) throws IOException {
         // WebSocket connection closes
+        System.out.println("onClose::" + session.getId());
     }
 
     @OnError
     public void onError(Session session, Throwable throwable) throws IOException {
         // Do error handling here
+        System.out.println("onError::" + throwable.getMessage());
     }
     
 }
