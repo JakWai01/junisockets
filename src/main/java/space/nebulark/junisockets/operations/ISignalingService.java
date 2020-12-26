54b509497e719137a143c00b6e5a039689fec4d9
JakWai01 <jakob.waibel@web.de>
2020-12-26 09:41:18 +0000

    feat: Add send methods to ISignalingService interface

--- src/main/java/space/nebulark/junisockets/operations/ISignalingService.java
+++ src/main/java/space/nebulark/junisockets/operations/ISignalingService.java
@@ -1,6 +1,24 @@
 package space.nebulark.junisockets.operations;
 
-public interface ISignalingService {
+import org.java_websocket.WebSocket;
+import org.java_websocket.WebSocketServerFactory;
+
+public interface ISignalingService extends WebSocket {
    
+    private static void send(WebSocket conn, Acknowledgement operation){};
+
+    private static void send(WebSocket conn, Offer operation){};
+
+    public static void send(WebSocket conn, Answer operation){};
+
+    public static void send(WebSocket conn, Candidate operation){};
+
+    public static void send(WebSocket conn, Alias operation){};
+
+    public static void send(WebSocket conn, Accept operation){};
     
+    public static void send(WebSocket conn, Greeting operation){};
+
+    public void send(Goodbye operation);
+
 }
