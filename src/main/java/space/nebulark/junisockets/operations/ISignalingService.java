1debb459257606eb085ee384727a0b2c4347c25b
JakWai01 <jakob.waibel@web.de>
2020-12-27 17:35:51 +0000

    fix: make ISignalingService methods public

--- src/main/java/space/nebulark/junisockets/operations/ISignalingService.java
+++ src/main/java/space/nebulark/junisockets/operations/ISignalingService.java
@@ -4,20 +4,20 @@ import org.java_websocket.WebSocket;
 
 public interface ISignalingService {
    
-    private static void send(WebSocket conn, Acknowledgement operation){};
+    static void send(WebSocket conn, Acknowledgement operation){};
 
-    private static void send(WebSocket conn, Offer operation){};
+    static void send(WebSocket conn, Offer operation){};
 
-    public static void send(WebSocket conn, Answer operation){};
+    static void send(WebSocket conn, Answer operation){};
 
-    public static void send(WebSocket conn, Candidate operation){};
+    static void send(WebSocket conn, Candidate operation){};
 
-    public static void send(WebSocket conn, Alias operation){};
+    static void send(WebSocket conn, Alias operation){};
 
-    public static void send(WebSocket conn, Accept operation){};
+    static void send(WebSocket conn, Accept operation){};
     
-    public static void send(WebSocket conn, Greeting operation){};
+    static void send(WebSocket conn, Greeting operation){};
 
-    public void send(Goodbye operation);
+    void send(Goodbye operation);
 
 }
