fb00ba2305a5e61bd5bc7cffb2c2123a4172a4df
JakWai01 <jakob.waibel@web.de>
2020-12-29 16:29:45 +0000

    feat: Add env vars

--- src/main/java/space/nebulark/junisockets/services/SignalingServer.java
+++ src/main/java/space/nebulark/junisockets/services/SignalingServer.java
@@ -614,16 +614,20 @@ public class SignalingServer extends WebSocketServer {
 
     public static void main(String[] args) throws InterruptedException, IOException {
         BasicConfigurator.configure();
-
         int port = 8892;
+        String host = "localhost";
+
         try {
             port = Integer.parseInt(args[0]);
         } catch (Exception ex) {
         }
 
-        // hier einfach den anderen constructor callen und davor die addresse aus host
-        // und port zusammensetzen
-        SignalingServer s = new SignalingServer(port);
+        try {
+            host = args[1];
+        } catch (Exception ex) {
+        }
+
+        SignalingServer s = new SignalingServer(new InetSocketAddress(host, port));
         s.start();
         System.out.println("SignalingServer started on port: " + s.getPort());
 
