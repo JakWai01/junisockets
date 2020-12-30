54461d281030efca1691833fbfcfa6a76eb72b2d
JakWai01 <jakob.waibel@web.de>
2020-12-30 15:02:28 +0000

    feat: Add TCPAddressTest

--- src/main/java/space/nebulark/junisockets/TestRunner.java
+++ src/main/java/space/nebulark/junisockets/TestRunner.java
@@ -6,17 +6,24 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 import space.nebulark.junisockets.addresses.IPAddressTest;
+import space.nebulark.junisockets.addresses.TCPAddressTest;
 
 public class TestRunner {
    
     public static void main(String[] args) {
         BasicConfigurator.configure();
         Result result = JUnitCore.runClasses(IPAddressTest.class);
+        Result result2 = JUnitCore.runClasses(TCPAddressTest.class);
 
         for(Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
         }
 
+        for (Failure failure : result2.getFailures()) {
+            System.out.println(failure.toString());
+        }
+
         System.out.println(result.wasSuccessful());
+        System.out.println(result2.wasSuccessful());
     }
 }
---
+++ src/main/java/space/nebulark/junisockets/addresses/TCPAddressTest.java
@@ -0,0 +1,113 @@
+package space.nebulark.junisockets.addresses;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+
+import space.nebulark.junisockets.errors.PortAlreadyAllocatedError;
+import space.nebulark.junisockets.errors.SubnetDoesNotExist;
+import space.nebulark.junisockets.errors.SuffixDoesNotExist;
+import space.nebulark.junisockets.services.SignalingServer;
+
+public class TCPAddressTest {
+
+    @Test
+    public void testParseTCPAddress() {
+        Logger logger = Logger.getLogger(SignalingServer.class);
+        ReentrantLock mutex = new ReentrantLock();
+        ConcurrentHashMap<String, HashMap<Integer, List<Integer>>> subnets = new ConcurrentHashMap<String, HashMap<Integer, List<Integer>>>();
+        IPAddress ip = new IPAddress(logger, mutex, subnets);
+        TCPAddress tcp = new TCPAddress(logger, mutex, subnets, ip);
+
+        String tcpAddress = "127.0.0.0:1234";
+
+        Assert.assertEquals(2, tcp.parseTCPAddress(tcpAddress).length);
+        Assert.assertEquals("127.0.0.0", tcp.parseTCPAddress(tcpAddress)[0]);
+        Assert.assertEquals("1234", tcp.parseTCPAddress(tcpAddress)[1]);
+
+    }
+
+    @Test
+    public void testToTCPAddress() {
+        Logger logger = Logger.getLogger(SignalingServer.class);
+        ReentrantLock mutex = new ReentrantLock();
+        ConcurrentHashMap<String, HashMap<Integer, List<Integer>>> subnets = new ConcurrentHashMap<String, HashMap<Integer, List<Integer>>>();
+        IPAddress ip = new IPAddress(logger, mutex, subnets);
+        TCPAddress tcp = new TCPAddress(logger, mutex, subnets, ip);
+
+        String ipAddress = "127.0.0.0";
+        int port = 1234;
+
+        Assert.assertEquals("127.0.0.0:1234", tcp.toTCPAddress(ipAddress, port));
+    }
+
+    @Test
+    public void testCreateTCPAddress() throws SuffixDoesNotExist, SubnetDoesNotExist {
+        Logger logger = Logger.getLogger(SignalingServer.class);
+        ReentrantLock mutex = new ReentrantLock();
+        ConcurrentHashMap<String, HashMap<Integer, List<Integer>>> subnets = new ConcurrentHashMap<String, HashMap<Integer, List<Integer>>>();
+        IPAddress ip = new IPAddress(logger, mutex, subnets);
+        TCPAddress tcp = new TCPAddress(logger, mutex, subnets, ip);
+
+        String ipAddress = "127.0.0.0";
+        String subnet = "127.0.0";
+
+        // create IPAddress to add to subnets
+        ip.createIPAddress(subnet);
+
+        Assert.assertEquals("127.0.0.0:0", tcp.createTCPAddress(ipAddress));
+    }
+
+    @Test
+    public void testClaimTCPAddress() throws PortAlreadyAllocatedError, SubnetDoesNotExist {
+        Logger logger = Logger.getLogger(SignalingServer.class);
+        ReentrantLock mutex = new ReentrantLock();
+        ConcurrentHashMap<String, HashMap<Integer, List<Integer>>> subnets = new ConcurrentHashMap<String, HashMap<Integer, List<Integer>>>();
+        IPAddress ip = new IPAddress(logger, mutex, subnets);
+        TCPAddress tcp = new TCPAddress(logger, mutex, subnets, ip);
+
+        String tcpAddress = "127.0.0.0:0";
+
+        String[] partsTCPAddress = tcp.parseTCPAddress(tcpAddress);
+        String[] partsIPAddress = ip.parseIPAddress(partsTCPAddress[0]);
+
+        String subnet = String.join(".", partsIPAddress[0], partsIPAddress[1], partsIPAddress[2]);
+        String suffix = partsIPAddress[3];
+        
+        ip.createIPAddress(subnet);
+        tcp.claimTCPAddress(tcpAddress);
+
+        Assert.assertEquals(true, subnets.get(subnet).get(Integer.parseInt(suffix)).contains(Integer.parseInt(partsTCPAddress[1])));
+    }
+
+    @Test
+    public void testRemoveTCPAddress() throws PortAlreadyAllocatedError, SubnetDoesNotExist {
+        Logger logger = Logger.getLogger(SignalingServer.class);
+        ReentrantLock mutex = new ReentrantLock();
+        ConcurrentHashMap<String, HashMap<Integer, List<Integer>>> subnets = new ConcurrentHashMap<String, HashMap<Integer, List<Integer>>>();
+        IPAddress ip = new IPAddress(logger, mutex, subnets);
+        TCPAddress tcp = new TCPAddress(logger, mutex, subnets, ip);
+
+        String tcpAddress = "127.0.0.0:0";
+
+        String[] partsTCPAddress = tcp.parseTCPAddress(tcpAddress);
+        String[] partsIPAddress = ip.parseIPAddress(partsTCPAddress[0]);
+
+        String subnet = String.join(".", partsIPAddress[0], partsIPAddress[1], partsIPAddress[2]);
+        String suffix = partsIPAddress[3];
+
+        ip.createIPAddress(subnet);
+        tcp.claimTCPAddress(tcpAddress);
+
+        // remove it again
+        tcp.removeTCPAddress(tcpAddress);
+        
+        Assert.assertEquals(false, subnets.get(subnet).get(Integer.parseInt(suffix)).contains(Integer.parseInt(partsTCPAddress[1])));
+    }
+
+}
