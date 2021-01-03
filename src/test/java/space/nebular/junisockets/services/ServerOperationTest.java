4f27850e041e7217444162517937dfa21181aa4e
JakWai01 <jakob.waibel@web.de>
2021-01-03 16:13:01 +0000

    feat: Add testHandleAcceptingRejected

--- src/test/java/space/nebular/junisockets/services/ServerOperationTest.java
+++ src/test/java/space/nebular/junisockets/services/ServerOperationTest.java
@@ -681,11 +681,10 @@ public class ServerOperationTest {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
-                    Assert.assertEquals(true,s.aliases.get((String)((JSONObject) operation.get("data")).get("alias")).getAccepting());
+                    Assert.assertEquals(true,
+                            s.aliases.get((String) ((JSONObject) operation.get("data")).get("alias")).getAccepting());
                     close();
                 }
-                
-                
 
             }
 
@@ -697,5 +696,98 @@ public class ServerOperationTest {
 
         cc.run();
         s.stop();
+    }
+
+    @Test
+    public void testHandleAcceptingRejected() throws URISyntaxException, IOException, InterruptedException {
+        PropertyConfigurator.configure("log4j.properties");
+        int port = 8892;
+        String host = "localhost";
+        Logger logger = Logger.getLogger(SignalingServer.class);
+
+        SignalingServerBuilder builder = new SignalingServerBuilder();
+
+        SignalingServer s = builder.setHost(host).setLogger(logger).setPort(port).build();
+
+        ReentrantLock mutex = new ReentrantLock();
+        IPAddress ip = new IPAddress(logger, mutex, s.subnets);
+        TCPAddress tcp = new TCPAddress(logger, mutex, s.subnets, ip);
+
+        String tcpAddress = "127.0.0.0:0";
+
+        String[] partsTCPAddress = tcp.parseTCPAddress(tcpAddress);
+        String[] partsIPAddress = ip.parseIPAddress(partsTCPAddress[0]);
+
+        String subnet = String.join(".", partsIPAddress[0], partsIPAddress[1], partsIPAddress[2]);
+
+        ip.createIPAddress(subnet);
+
+        s.start();
+        WebSocketClient cc = new WebSocketClient(new URI("ws://localhost:8892")) {
+
+            @Override
+            public void onError(Exception ex) {
+                ex.printStackTrace();
+            }
+
+            @Override
+            public void onClose(int code, String reason, boolean remote) {
+            }
+
+            @Override
+            public void onMessage(String message) {
+                JSONParser parser = new JSONParser();
+                Object jsonObj = null;
+
+                try {
+                    jsonObj = parser.parse(message);
+                } catch (org.json.simple.parser.ParseException e) {
+                    e.printStackTrace();
+                }
+
+                JSONObject operation = (JSONObject) jsonObj;
+
+                if (operation.get("opcode").equals(ESignalingOperationCode.ACKNOWLEDGED.getValue())) {
+                    send("{\"data\":{\"id\":\"127.0.0.0\",\"alias\":\"127.0.0.0:0\"},\"opcode\":\"accepting\"}");
+
+                    try {
+                        Thread.sleep(300);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                    Assert.assertEquals(false, s.aliases.containsKey("127.0.0.0:0"));
+                    close();
+                }
+
+                // if (operation.get("opcode").equals(ESignalingOperationCode.ALIAS.getValue()))
+                // {
+                // System.out.println(s.aliases.toString());
+                // send("{\"data\":{\"id\":\"127.0.0.0\",\"alias\":\"127.0.0.0:0\"},\"opcode\":\"accepting\"}");
+                // try {
+                // // We should replace that somehow
+                // Thread.sleep(300);
+                // } catch (InterruptedException e) {
+                // // TODO Auto-generated catch block
+                // e.printStackTrace();
+                // }
+                // Assert.assertEquals(true,
+                // s.aliases.get((String) ((JSONObject)
+                // operation.get("data")).get("alias")).getAccepting());
+                // close();
+                // }
+
+            }
+
+            @Override
+            public void onOpen(ServerHandshake handshakedata) {
+                send("{\"data\":{\"subnet\":\"127.0.0\"},\"opcode\":\"knock\"}");
+            }
+        };
+
+        cc.run();
+        s.stop();
+        ;
     }
 }
